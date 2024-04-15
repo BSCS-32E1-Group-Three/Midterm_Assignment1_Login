4115028659cd44fdc705508c432c42640556d097
Peñaflor_Jose_BSCS32E1 <160807078+Penaflor-Jose-BSCS32E1@users.noreply.github.com>
2024-04-15 17:46:55 +0800

    fixed service error

--- Midterm_Assignment1_Login/Program.cs
+++ Midterm_Assignment1_Login/Program.cs
@@ -1,7 +1,12 @@
+
+using Midterm_Assignment1_Login.Data;
+using Midterm_Assignment1_Login.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
